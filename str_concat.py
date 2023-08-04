a3ea9192d3b09f8fff4ac6cf54990259637fd23c
Saad985 <shafayamjad49@gmail.com>
2023-08-05 00:04:04 +0500

    full name

---
+++ str_concat.py
@@ -0,0 +1,9 @@
+#string concatenation
+first_name="Muhammad Saad"
+last_name="Amjad"
+
+full_name=first_name+last_name           #without space
+print(full_name)
+
+full_name=first_name +" "+ last_name       #with space
+print (full_name)
