62cb0320701fcde41fa98bed0ea44065e472e800
Saad985 <shafayamjad49@gmail.com>
2023-08-05 00:21:38 +0500

    addition of string with an int number

--- str_concat.py
+++ str_concat.py
@@ -7,3 +7,15 @@ print(full_name)
 
 full_name=first_name +" "+ last_name       #with space
 print (full_name)
+
+#full_name=first_name+3     #error
+#print(full_name)
+
+full_name=first_name+"3"         #no error,withoutspace
+print(full_name)
+
+full_name=first_name+str(3)       #no error,without space
+print (full_name)
+
+full_name=first_name+" "+str(3)          #no error,with space
+print (full_name)
